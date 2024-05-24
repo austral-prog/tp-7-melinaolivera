49a2342f095f2eae2f773ee1d9889faaa16d20df
melinaolivera <162723084+melinaolivera@users.noreply.github.com>
2024-05-23 22:34:34 -0300

    Update loops_and_print.py

--- loops_and_print.py
+++ loops_and_print.py
@@ -1,21 +1,19 @@
    def enumerate_list(strings):
     result = []
     for index, value in enumerate(strings):
-        if value:  # Si el valor no es una cadena vacía
+        if value:  
             result.append(f"{index}. {value}")
     return result
 
-# Ejemplo de uso
 colors = ["Red", "Green", "", "White", "Black"]
-print(enumerate_list(colors))  # ["0. Red", "1. Green", "3. White", "4. Black"]
+print(enumerate_list(colors))  
 
 def enumerate_backwards(strings):
     result = []
     for index, value in enumerate(strings):
-        if value:  # Si el valor no es una cadena vacía
+        if value: 
             result.append(f"{index}. {value[::-1]}")
     return result
 
-# Ejemplo de uso
 colors = ["Red", "Green", "", "White", "Black"]
-print(enumerate_backwards(colors))  # ["0. deR", "1. neerG", "3. etihW", "4. kcalB"]
+print(enumerate_backwards(colors)) 
