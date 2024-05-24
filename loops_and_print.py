5391b8010262efd07a12abcd4328fff5381df4ba
melinaolivera <162723084+melinaolivera@users.noreply.github.com>
2024-05-23 22:29:42 -0300

    Update loops_and_print.py

--- loops_and_print.py
+++ loops_and_print.py
@@ -1,22 +1,21 @@
-   def enumerate_list(list):
+   def enumerate_list(strings):
+    result = []
+    for index, value in enumerate(strings):
+        if value:  # Si el valor no es una cadena vacía
+            result.append(f"{index}. {value}")
+    return result
 
-    lists = []
-    lists2 = []
-    for e in list:
-        if e != "":
-            lists.append(e)
-    for i, e in enumerate(lists[0:]):
-        lists2.append(f"{i}. {e}")
-    return lists2
+# Ejemplo de uso
+colors = ["Red", "Green", "", "White", "Black"]
+print(enumerate_list(colors))  # ["0. Red", "1. Green", "3. White", "4. Black"]
 
-#retorna la lista:
-# ["0. Red", "1. Green", "2. White", "3. Black"]
-def enumerate_backwards(list):
-    lists = []
-    lists2 = []
-    for e in list:
-        if e != "":
-            lists.append(e[::-1])
-    for i, e in enumerate(lists[0:]):
-        lists2.append(f"{i}. {e}")
-    return lists2
+def enumerate_backwards(strings):
+    result = []
+    for index, value in enumerate(strings):
+        if value:  # Si el valor no es una cadena vacía
+            result.append(f"{index}. {value[::-1]}")
+    return result
+
+# Ejemplo de uso
+colors = ["Red", "Green", "", "White", "Black"]
+print(enumerate_backwards(colors))  # ["0. deR", "1. neerG", "3. etihW", "4. kcalB"]
