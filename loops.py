e9abd324033045ed57d5624d68e02e9862aa81b0
melinaolivera <162723084+melinaolivera@users.noreply.github.com>
2024-05-23 22:36:03 -0300

    Update loops.py

--- loops.py
+++ loops.py
@@ -1,12 +1,9 @@
-def index_of_by_index(word, my_list, start_index):
-    if word not in my_list[start_index:]:
-        return -1
+def index_of_by_index(word, list, index):
+    if not word in list[index:]: return -1
     
-    for i, e in enumerate(my_list[start_index:], start=start_index):
+    for i, e in enumerate(list[index:]):
         if word == e:
-            return i
-    return -1
-
+            return i + len(list[:index])
 
 
 def index_of_empty(list):
@@ -16,36 +13,36 @@ def index_of_empty(list):
             break
     return i
 
+        
 
-def index_of(word, my_list):
-    if word not in my_list:
-        return -1
-    for i, e in enumerate(my_list):
+
+
+def index_of(word, list):
+    
+    if not word in list: return -1
+    for i, e in enumerate(list):
         if e == word:
-            return i
-    return -1
+            break
+    return i
+    
 
 #colors = ["Red", "Green", "White", "Black", "Pink", "Yellow", "Black"]
-
-
-def put(word, my_list):
-    if "" not in my_list:
-        return -1
-    for i, e in enumerate(my_list):
-        if e == "":
-            my_list[i] = word
-            return i
-    return -1
+def put(word, list):
+    if not "" in list: return -1 #si no hay un "" en la list directo que ponga -1
+    for i, e in enumerate(list): # i = index de la palabra buscada, e = elemento ubicado en ese index
+        if e == "": # defino para que frene en el momento en el que encuentre un "" y ahi hace o de abajo
+            break
+    list[i] = word #reemplazo en el index que esta el ""(vacio) por la word
+    return i #returnea la index
     
 
 
-def remove(word, my_list):
-    if word not in my_list:
-        return 0
-    count = 0
-    for i, e in enumerate(my_list):
+def remove(word, list):
+    if not word in list: return 0 # si la word no esta en list directo te pone 0
+    count = 0 # cantidad de words que hay
+    for i, e in enumerate(list): 
         if e == word:
-            my_list[i] = ""
-            count += 1
+            list[i], count = "", count +1 # En la posicion i, se reemplaza por count = "" y al pasar 1 vez se suma uno, 
+                                          # y al pasar otra vez se cambia denuevo por la posicion de la otra palabra y 
+                                          # se le suma 1 mas a su count, osea va a decir 2
     return count
- 
