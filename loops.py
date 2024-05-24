db9123c79470711f6fc0c567a21739a3190f93e7
melinaolivera <162723084+melinaolivera@users.noreply.github.com>
2024-05-23 22:31:44 -0300

    Update loops.py

--- loops.py
+++ loops.py
@@ -1,23 +1,51 @@
- def enumerate_list(list):
+def index_of_by_index(word, my_list, start_index):
+    if word not in my_list[start_index:]:
+        return -1
+    
+    for i, e in enumerate(my_list[start_index:], start=start_index):
+        if word == e:
+            return i
+    return -1
 
-    lists = []
-    lists2 = []
-    for e in list:
-        if e != "":
-            lists.append(e)
-    for i, e in enumerate(lists[0:]):
-        lists2.append(f"{i}. {e}")
-    return lists2
 
-#retorna la lista:
-# ["0. Red", "1. Green", "2. White", "3. Black"]
-def enumerate_backwards(list):
-    lists = []
-    lists2 = []
-    for e in list:
-        if e != "":
-            lists.append(e[::-1])
-    for i, e in enumerate(lists[0:]):
-        lists2.append(f"{i}. {e}")
-    return lists2
-[21:28, 23/5/2024] felicitas piñeiro: 
+
+def index_of_empty(list):
+    if not "" in list: return -1
+    for i, e in enumerate(list):
+        if e == "":
+            break
+    return i
+
+
+def index_of(word, my_list):
+    if word not in my_list:
+        return -1
+    for i, e in enumerate(my_list):
+        if e == word:
+            return i
+    return -1
+
+#colors = ["Red", "Green", "White", "Black", "Pink", "Yellow", "Black"]
+
+
+def put(word, my_list):
+    if "" not in my_list:
+        return -1
+    for i, e in enumerate(my_list):
+        if e == "":
+            my_list[i] = word
+            return i
+    return -1
+    
+
+
+def remove(word, my_list):
+    if word not in my_list:
+        return 0
+    count = 0
+    for i, e in enumerate(my_list):
+        if e == word:
+            my_list[i] = ""
+            count += 1
+    return count
+ 
